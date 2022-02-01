c9f98f8add77b30ed56f16eeff16b42029d042a3
Hunter Bennett <hunter.ben28@hotmail.com>
2022-02-01 16:07:39 -0500

    Removed Bulky Libraries
    Removed the MUI library as it was causing memory overloads on
    the free tier of Heroku. Icons were returned back to SVGs.

--- src/components/EndOptions.tsx
+++ src/components/EndOptions.tsx
@@ -1,6 +1,4 @@
-import ReplayIcon from '@mui/icons-material/Replay';
-import SettingsIcon from '@mui/icons-material/Settings';
-import { IconButton } from '@mui/material';
+import { CheckMark, FailureX, SettingsIcon, ReplayIcon } from '../resources/svgs'
 
 export interface EndOptionsProps {
     success: boolean
@@ -24,34 +22,18 @@ const EndOptions = ({ success, word, changeSettings, replay }: EndOptionsProps) 
     return (
         <div className='endOptionsContainer'>
             <div className='iconButton' onClick={() => changeSettings()}>
-                <IconButton sx={{ color: 'white' }}>
-                    <SettingsIcon fontSize='large'/>
-                </IconButton>
+                { SettingsIcon }
                 <p>Settings</p>
             </div>
             <div className='result'>
-                { !success && <p>{word.toUpperCase()}</p> }
                 <div className={`resultIconContainer ${success ? 'success' : 'failure'}`}>
                     {/* Render the result icon based on the success of the game */}
-                    {success ? (
-                        <svg className='checkmark' viewBox='0 0 25 30'>
-                            <path d='M2,19.2C5.9,23.6,9.4,28,9.4,28L23,2' />
-                        </svg>
-                    ) : (
-                        <svg
-                            className='failureX'
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 24 24'
-                        >
-                            <path d='M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z' />
-                        </svg>
-                    )}
+                    { success ? CheckMark : FailureX }
                 </div>
+                {!success && <p>{word.toUpperCase()}</p>}
             </div>
             <div className='iconButton' onClick={() => replay()}>
-                <IconButton sx={{ color: 'white' }}>
-                    <ReplayIcon fontSize='large'/>
-                </IconButton>
+                { ReplayIcon }
                 <p>Play Again</p>
             </div>
         </div>
